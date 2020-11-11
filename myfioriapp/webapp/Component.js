6735a61a847d3560fd1d2ea702820bddc360ec33
bharatbjj <bharatbjj@gmail.com>
2020-11-12 00:52:05 +0800

    commit

--- myfioriapp/webapp/Component.js
+++ myfioriapp/webapp/Component.js
@@ -1,7 +1,7 @@
 sap.ui.define(['sap/suite/ui/generic/template/lib/AppComponent'], function(AppComponent) {
-    return AppComponent.extend('myapp.bb.test.myfioriapp.Component', {
-        metadata: {
-            manifest: 'json'
-        }
-    });
+	return AppComponent.extend('myapp.bb.test.myfioriapp.Component', {
+		metadata: {
+			manifest: 'json'
+		}
+	});
 });
